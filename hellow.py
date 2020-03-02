2b50109af1b8103f7e86c9923ab3f6efe61b1b6b
million_luo <mrbigsoap@163.com>
2020-03-02 13:27:38 +0800

    Update hellow.py

---
+++ hellow.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   hellow.py
+@Time    :   2020/03/02 13:27:04
+@Author  :   million_luo
+@Version :   1.0
+'''
+
+import os
+import sys
+
+print("hellow worlw")
